ae238e7e32d66d5a8ca37a4c31e980d7d713267d
BuildTools <unconfigured@null.spigotmc.org>
2024-06-27 15:25:05 +0300

    Initial commit

---
+++ src/main/java/net/springer29/shadowrealm/ShadowRealmClient.java
@@ -0,0 +1,10 @@
+package net.springer29.shadowrealm;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ShadowRealmClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
